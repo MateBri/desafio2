92dce1e7c959b5bb5a1fda4945ab7d6d39ddb8ca
Matebri <mateobrichetti@gmail.com>
2024-04-18 19:52:38 -0300

    hola

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let nombreProducto = "Helader"
+let precioUnitario = 10
+let cantidadDeseada = prompt("Ingrese la cantida de heladeras que quiere")
+
+let costoFinal = precioUnitario * cantidadDeseada
+
+alert(costoFinal)
